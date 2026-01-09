ca0b6268f82bb5964a5b96f7a76332282f51d347
Simon Lynch <simon.lynch@hashicorp.com>
2026-01-09 11:28:47 +1100

    refactor: remove debug output definitions for nodes

--- bcm_cmkube_cluster/main.tf
+++ bcm_cmkube_cluster/main.tf
@@ -42,41 +42,6 @@ output "networks" {
   value = data.bcm_cmnet_networks.all.networks
 }
 
-# Debug outputs - show all discovered nodes
-output "all_nodes" {
-  description = "All nodes discovered from BCM"
-  value = {
-    count     = length(data.bcm_cmdevice_nodes.all.nodes)
-    hostnames = [for node in data.bcm_cmdevice_nodes.all.nodes : node.hostname]
-  }
-}
-
-output "etcd_nodes" {
-  description = "Filtered etcd nodes"
-  value = {
-    count     = length(local.etcd_nodes)
-    hostnames = [for node in local.etcd_nodes : node.hostname]
-    uuids     = [for node in local.etcd_nodes : node.uuid]
-  }
-}
-
-output "worker_nodes" {
-  description = "Filtered worker nodes"
-  value = {
-    count     = length(local.worker_nodes)
-    hostnames = [for node in local.worker_nodes : node.hostname]
-    uuids     = [for node in local.worker_nodes : node.uuid]
-  }
-}
-
-output "master_nodes" {
-  description = "Filtered master nodes"
-  value = {
-    count     = length(local.master_nodes)
-    hostnames = [for node in local.master_nodes : node.hostname]
-    uuids     = [for node in local.master_nodes : node.uuid]
-  }
-}
 
 # Create the Kubernetes cluster based on cm-kubernetes-setup.conf
 resource "bcm_cmkube_cluster" "terraform" {
