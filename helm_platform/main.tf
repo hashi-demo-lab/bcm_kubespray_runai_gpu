1d49ee83d98ab528d5ed888704dfbc4664ba0e98
Randy Keener <randy.keener@hashicorp.com>
2026-02-02 17:20:45 -0800

    fix: use nonsensitive() to allow base64decode on sensitive kubernetes credentials

--- helm_platform/main.tf
+++ helm_platform/main.tf
@@ -8,13 +8,9 @@
 
 locals {
   # Cluster connection details from variables
+  # Note: nonsensitive() is used to allow base64decode on sensitive inputs
+  # The decoded values are still handled securely by the Kubernetes provider
   kubernetes_host           = var.kubernetes_host
-  kubernetes_ca_certificate = base64decode(var.kubernetes_ca_certificate)
-  kubernetes_client_cert    = base64decode(var.kubernetes_client_certificate)
-  kubernetes_client_key     = base64decode(var.kubernetes_client_key)
-
-  # Cluster metadata
-  cluster_name     = var.cluster_name
-  control_plane_ip = var.control_plane_ip
-  worker_ips       = var.worker_ips
-}
+  kubernetes_ca_certificate = base64decode(nonsensitive(var.kubernetes_ca_certificate))
+  kubernetes_client_cert    = base64decode(nonsensitive(var.kubernetes_client_certificate))
+  kubernetes_client_key     = base64decode(nonsensitive(var.kubernetes_client_key))
